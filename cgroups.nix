71c2d12a2264d526ce7f7eb8ccdae1b5e6e54b7d
Hoang Xuan Phu <phuhimself@phunehehe.net>
2014-06-07 23:39:52 +0700

    add a basic service

--- cgroups.nix
+++ cgroups.nix
@@ -12,4 +12,17 @@
         haskellPackages.HTTP
         haskellPackages.text
     ];
+
+
+    systemd.services.cgroups-manager =
+        let site_dir = "/cgroups";
+        in {
+        description = "CGroups Manager";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+        serviceConfig = {
+            ExecStart = "${site_dir}/dist/build/cgroups-manager/cgroups-manager";
+            Restart = "always";
+        };
+    };
 }
