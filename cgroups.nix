91f5c5db0b87db1f5fca984b094afb1d672f7dbf
Hoang Xuan Phu <phuhimself@phunehehe.net>
2014-06-08 13:54:03 +0700

    proxy via nginx

--- cgroups.nix
+++ cgroups.nix
@@ -8,6 +8,24 @@ in {
     nixpkgs.config.allowUnfree = true;
     environment.systemPackages = [ cgroupsManager ];
 
+    services.nginx.enable = true;
+    services.nginx.httpConfig = ''
+        types {
+            text/html                html;
+            text/css                 css;
+            application/x-javascript js;
+        }
+        server {
+            root /cgroups/static;
+            location / {
+                try_files $uri @happstack;
+            }
+            location @happstack {
+                proxy_pass http://localhost:8000;
+            }
+        }
+    '';
+
     systemd.services.cgroups-manager = {
         description = "CGroups Manager";
         wantedBy = [ "multi-user.target" ];
